58d8b0b4e08de7a5e9f49b450f0a035ae2272ef2
Krzysztof <krzysztof.andrzej.walczak@gmail.com>
2015-03-21 13:57:35 +0100

    added collaborator

--- src/main/java/waw/decision/maker/DecisionMakerController.java
+++ src/main/java/waw/decision/maker/DecisionMakerController.java
@@ -1,7 +1,7 @@
 package waw.decision.maker;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import waw.decision.maker.model.LoanApplication;
 
 /**
  * Created by kwalczak on 21.03.15.
@@ -9,9 +9,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DecisionMakerController {
 
+    private static final String FRAUD = "fraud";
+    private static final String GOOD = "good";
+    private static final String FISHY = "fishy";
+
+    private static final String FAILURE = "failure";
+    private static final String MANUAL = "manual";
+    private static final String SUCCESS = "success";
+
 
     @RequestMapping("/test")
     public String greeting() {
-        return  "working";
+        return "working";
+    }
+
+    @RequestMapping(value = "/api/loanApplication/{loanApplicationId}", method = RequestMethod.PUT)
+    public void decide(@PathVariable String loanApplicationId, @RequestBody LoanApplication loanApplication) {
+        String loanStatus;
+        switch (loanApplication.getFraudStatus()) {
+            case FRAUD:
+                loanStatus = FAILURE;
+            case GOOD:
+                loanStatus = SUCCESS;
+            case FISHY:
+                loanStatus = MANUAL;
+            default:
+                loanStatus = MANUAL;
+
+        }
+        //decide
+        //insert to db
+        //call report
     }
 }
